3010f14571a53099e7bf5d53d1ccf4384db2c28a
Jun Qing <junqingchang@hotmail.com>
2017-10-31 12:06:40 +0800

    Add files via upload

---
+++ Project-2D-Algo/src/sat/SATSolverTest.java
@@ -0,0 +1,81 @@
+package sat;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+/*
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+*/
+
+import sat.env.*;
+import sat.formula.*;
+
+
+public class SATSolverTest {
+    Literal a = PosLiteral.make("a");
+    Literal b = PosLiteral.make("b");
+    Literal c = PosLiteral.make("c");
+    Literal na = a.getNegation();
+    Literal nb = b.getNegation();
+    Literal nc = c.getNegation();
+
+
+
+	
+	// TODO: add the main method that reads the .cnf file and calls SATSolver.solve to determine the satisfiability
+    public static void main(String[] aregs){
+    	InputStream inputStream = new FileInputStream("");
+    	Reader inputStreamReader = new InputStreamReader(inputStream);
+    	
+    	int data = inputStreamReader.read();
+    	while (data != -1){
+    		char theChar = (char)data;
+    		data = inputStreamReader.read();
+    		
+    	}
+    	
+    }
+	
+    public void testSATSolver1(){
+    	// (a v b)
+    	Environment e = SATSolver.solve(makeFm(makeCl(a,b))	);
+/*
+    	assertTrue( "one of the literals should be set to true",
+    			Bool.TRUE == e.get(a.getVariable())  
+    			|| Bool.TRUE == e.get(b.getVariable())	);
+    	
+*/    	
+    }
+    
+    
+    public void testSATSolver2(){
+    	// (~a)
+    	Environment e = SATSolver.solve(makeFm(makeCl(na)));
+/*
+    	assertEquals( Bool.FALSE, e.get(na.getVariable()));
+*/    	
+    }
+    
+    private static Formula makeFm(Clause... e) {
+        Formula f = new Formula();
+        for (Clause c : e) {
+            f = f.addClause(c);
+        }
+        return f;
+    }
+    
+    private static Clause makeCl(Literal... e) {
+        Clause c = new Clause();
+        for (Literal l : e) {
+            c = c.add(l);
+        }
+        return c;
+    }
+    
+    
+    
+}
