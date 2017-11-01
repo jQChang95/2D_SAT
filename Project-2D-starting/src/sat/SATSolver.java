3ff709365821bc5b50eee384221a873b47d1b234
Jun Qing <junqingchang@hotmail.com>
2017-11-01 12:21:46 +0800

    Add files via upload

--- Project-2D-starting/src/sat/SATSolver.java
+++ Project-2D-starting/src/sat/SATSolver.java
@@ -1,7 +1,9 @@
 package sat;
 
+import immutable.EmptyImList;
 import immutable.ImList;
 import sat.env.Environment;
+import sat.env.Variable;
 import sat.formula.Clause;
 import sat.formula.Formula;
 import sat.formula.Literal;
@@ -20,8 +22,10 @@ public class SATSolver {
      *         null if no such environment exists.
      */
     public static Environment solve(Formula formula) {
-        // TODO: implement this.
-        throw new RuntimeException("not yet implemented.");
+        // this calls the next function
+    	ImList<Clause> clauses = formula.getClauses();
+    	Environment env = new Environment();
+    	return solve(clauses, env);
     }
 
     /**
@@ -37,8 +41,71 @@ public class SATSolver {
      *         or null if no such environment exists.
      */
     private static Environment solve(ImList<Clause> clauses, Environment env) {
-        // TODO: implement this.
-        throw new RuntimeException("not yet implemented.");
+    	//If there are no clauses, the formula is trivially satisfiable.
+    	//• If there is an empty clause, the clause list is unsatisfiable -- fail and backtrack. (use empty
+    	//	clause to denote a clause evaluated to FALSE based on the variable binding in the
+    	//	environment)
+    	if (clauses.isEmpty()){
+    		return env;
+    	}
+    	//empty clause
+    	for (Clause clause : clauses){
+    		if (clause.isEmpty()){
+    			return null;
+    		}
+    	}
+
+    	//• Otherwise, find the smallest clause (by number of literals).
+    	Clause smallestClause = new Clause();
+    	int smallestSize = 99999999;
+    	for (Clause clause: clauses){
+    		if (clause.size()<smallestSize){
+    			smallestSize = clause.size();
+    			smallestClause = clause;
+    		}
+    		if (smallestSize == 1){
+    			break;	
+    		}
+    	}
+    	
+    	//	o If the clause has only one literal, bind its variable in the environment so that the
+    	//	clause is satisfied, substitute for the variable in all the other clauses (using the
+    	//	suggested substitute() method), and recursively call solve().
+    	//	o Otherwise, pick an arbitrary literal from this small clause:
+    	//		First try setting the literal to TRUE, substitute for it in all the clauses, then
+    	//		solve() recursively.
+    	//		If that fails, then try setting the literal to FALSE, substitute, and solve()
+    	//		recursively.
+
+    	Environment newEnv = new Environment(); //env to put the var as true
+    	Literal literal = smallestClause.chooseLiteral();
+    	String checker = literal.toString();
+    	Variable var = literal.getVariable();	
+    	Environment output = new Environment(); //output
+    	ImList<Clause> newClauses = new EmptyImList<Clause>(); //Substituted list
+    	if (checker.startsWith("~")){
+    		newEnv = env.putFalse(var);
+    	}else{
+    		newEnv = env.putTrue(var);
+    	}
+    	newClauses = substitute(clauses, literal);
+    	if (smallestClause.isUnit()){
+    		output = solve(newClauses, newEnv);
+    	}else{
+    		output = solve(newClauses, newEnv);
+    		if (output == null){
+    	    	if (checker.startsWith("~")){
+    	    		newEnv = env.putTrue(var);
+    	    	}else{
+    	    		newEnv = env.putFalse(var);
+    	    	}
+    			Literal nLiteral = literal.getNegation();
+    			newClauses = substitute(clauses, nLiteral);
+    			output = solve(newClauses, newEnv);
+    		}
+    	}
+    	return output;
+    	
     }
 
     /**
@@ -53,8 +120,22 @@ public class SATSolver {
      */
     private static ImList<Clause> substitute(ImList<Clause> clauses,
             Literal l) {
-        // TODO: implement this.
-        throw new RuntimeException("not yet implemented.");
+        // Parse through all the clauses in the ImList and change the literal l to be true using reduce.
+    	// If newClause comes out as not null (means still not true, because if true we can ignore) then add it to the new ImList
+    	Clause newClause = new Clause();
+    	ImList<Clause> outClauses = new EmptyImList<Clause>();
+    	if (clauses.isEmpty()){
+    		return outClauses;
+    	}
+    	for (Clause clause : clauses){
+    		if (!clause.isEmpty() && clause != null){
+    			newClause = clause.reduce(l);
+    		}
+    		if (newClause != null){
+    			outClauses = outClauses.add(newClause);
+    		}
+    	}
+    	return outClauses;
     }
 
 }
--- Project-2D-starting/src/sat/SATSolverTest.java
+++ Project-2D-starting/src/sat/SATSolverTest.java
@@ -6,6 +6,11 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 */
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 import sat.env.*;
 import sat.formula.*;
 
@@ -17,33 +22,114 @@ public class SATSolverTest {
     Literal na = a.getNegation();
     Literal nb = b.getNegation();
     Literal nc = c.getNegation();
-
-
-
-	
-	// TODO: add the main method that reads the .cnf file and calls SATSolver.solve to determine the satisfiability
-    public static void main(String[] aregs){
-    	
+    
+    
+    
+    
+    // TODO: add the main method that reads the .cnf file and calls SATSolver.solve to determine the satisfiability
+    public static void main(String args[]) throws IOException {
+        System.out.println("Reading File");
+    	long readTime = System.nanoTime();
+    	File fin = null;
+        Scanner bin = null;
+        
+        try {
+            fin=new File("Sg.cnf"); //import file
+            bin=new Scanner(fin);
+            
+            String line;
+            bin.nextLine();//remove the commented part
+            String[] format=bin.nextLine().split(" ");
+            int NumberOfClauses=Integer.parseInt(format[3]);//get the number of clauses
+            Formula f = new Formula(); //create and instance of the formula
+            while (f.getSize()!=NumberOfClauses) {
+                line=bin.nextLine();
+                if(line.length()>0){
+                    String[] tempLine=line.split(" ");
+                    Clause c = new Clause();
+                    
+                    for(String i:tempLine){
+                        if(Integer.parseInt(i)==0){
+                            break;
+                        }
+                        Literal literal = PosLiteral.make(Integer.toString(Math.abs(Integer.parseInt(i))));//makes literal instance
+                        
+                        
+                        if((Integer.parseInt(i))<0){ //add the negated Integer to the clause if string is negative
+                            c=c.add(literal.getNegation());
+                        }
+                    else if ((Integer.parseInt(i))>0){ //add the postitive Integer to the clause if string is positive
+                            c=c.add(literal);
+                        }
+                    }
+                    f=f.addClause(c); //add the clauses to the formula
+                    
+                    
+                }
+            }
+            String fileName = "D:/School/ISTD/Java/2D/BoolAssignment.txt";
+            PrintWriter write = new PrintWriter(fileName, "UTF-8");
+            long endReadTime = System.nanoTime();
+            long tReadTime = endReadTime - readTime;
+            System.out.println("Reading Time: " + tReadTime/1000000000.0 + "s");
+            System.out.println("SAT Solver starts");
+            long started = System.nanoTime();
+            Environment env = SATSolver.solve(f);
+            long time = System.nanoTime();
+            long timeTaken = time - started;
+            System.out.println("Solving Time: " + timeTaken/1000000.0 + "ms");
+            System.out.println("Total Time: " + (timeTaken+tReadTime)/1000000000.0 + "s");
+            if (env == null){
+            	System.out.println("Formula Unsatisfiable");
+            }else{
+            	System.out.println("Formula Satisfiable");
+            	String bindings = env.toString();
+            	bindings = bindings.substring(bindings.indexOf("[")+1, bindings.indexOf("]"));
+            	String[] bindingNew = bindings.split(", ");
+            	for (String binding : bindingNew){
+            		String[] bind = binding.split("->");
+            		write.println(bind[0] + ":" + bind[1]);
+            	}
+            	
+            	System.out.println(bindings);
+            }
+            write.close();
+            //TO BE REMOVED!!!!! WILL SLOW DOWN THE CODE===========================================
+//            if(NumberOfClauses==f.getSize()){
+//                System.out.println("Fomula generated Successfully!");
+//            }
+//            else{
+//                System.out.println("ExpectedNumberOfClauses: "+ NumberOfClauses);
+//                System.out.println("FormulaSize: "+ f.getSize());
+//            }
+//            System.out.println(f.toString());
+            //====================================================================================
+            
+        }finally {
+            if (bin != null) {
+                bin.close();
+                
+            }
+        }
     }
-	
     public void testSATSolver1(){
-    	// (a v b)
-    	Environment e = SATSolver.solve(makeFm(makeCl(a,b))	);
-/*
-    	assertTrue( "one of the literals should be set to true",
-    			Bool.TRUE == e.get(a.getVariable())  
-    			|| Bool.TRUE == e.get(b.getVariable())	);
-    	
-*/    	
+        // For example , (a v b):
+        Environment e = SATSolver.solve(makeFm(makeCl(a,b)));
+        /*
+        assertTrue( "one of the literals should be set to true",
+        Bool.TRUE == e.get(a.getVariable())
+        || Bool.TRUE == e.get(b.getVariable())	);
+        
+        */
     }
     
     
     public void testSATSolver2(){
-    	// (~a)
-    	Environment e = SATSolver.solve(makeFm(makeCl(na)));
-/*
-    	assertEquals( Bool.FALSE, e.get(na.getVariable()));
-*/    	
+        // (~a)
+        Environment e = SATSolver.solve(makeFm(makeCl(na)));
+        /*
+        assertEquals( Bool.FALSE, e.get(na.getVariable()));
+        */
     }
     
     private static Formula makeFm(Clause... e) {
